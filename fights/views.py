cb8cc3759ff617285a17394e159c8a359334366e
sivadeepkumar <sivadeepkumar3@gmail.com>
2024-06-04 02:01:16 +0530

    Added the fights app to integrate the manage.py

---
+++ fights/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+# chat/views.py
+
+from django.shortcuts import render
+
+from fights.models import Room
+
+
+def index_view(request):
+    return render(request, 'index.html', {
+        'rooms': Room.objects.all(),
+    })
+
+
+def room_view(request, room_name):
+    chat_room, created = Room.objects.get_or_create(name=room_name)
+    return render(request, 'room.html', {
+        'room': chat_room,
+    })
